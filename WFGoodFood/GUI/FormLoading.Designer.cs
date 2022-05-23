be948e1575f61587bbba1d9dca3d7d2707bf1a09
Yulia2120 <ket-dz@rambler.ru>
2022-05-23 21:09:11 +0300

    Color changed

--- WFGoodFood/GUI/FormLoading.Designer.cs
+++ WFGoodFood/GUI/FormLoading.Designer.cs
@@ -38,7 +38,7 @@
             // panLoad
             // 
             this.panLoad.Anchor = System.Windows.Forms.AnchorStyles.Top;
-            this.panLoad.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.panLoad.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(136)))), ((int)(((byte)(167)))), ((int)(((byte)(0)))));
             this.panLoad.Controls.Add(this.panMove);
             this.panLoad.Location = new System.Drawing.Point(182, 332);
             this.panLoad.Name = "panLoad";
@@ -47,7 +47,7 @@
             // 
             // panMove
             // 
-            this.panMove.BackColor = System.Drawing.Color.Red;
+            this.panMove.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(222)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
             this.panMove.Location = new System.Drawing.Point(186, 0);
             this.panMove.Name = "panMove";
             this.panMove.Size = new System.Drawing.Size(70, 5);
@@ -58,7 +58,7 @@
             this.label1.Anchor = System.Windows.Forms.AnchorStyles.Top;
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Century Gothic", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(85)))), ((int)(((byte)(115)))), ((int)(((byte)(0)))));
             this.label1.Location = new System.Drawing.Point(245, 275);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(128, 22);
@@ -69,7 +69,7 @@
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Sarllina", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(240)))), ((int)(((byte)(240)))), ((int)(((byte)(242)))));
+            this.label2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(85)))), ((int)(((byte)(115)))), ((int)(((byte)(0)))));
             this.label2.Location = new System.Drawing.Point(208, 129);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(204, 75);
@@ -80,7 +80,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(26)))), ((int)(((byte)(30)))), ((int)(((byte)(33)))));
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(240)))), ((int)(((byte)(240)))), ((int)(((byte)(242)))));
             this.ClientSize = new System.Drawing.Size(630, 450);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
--- WFGoodFood/GUI/FormLoading.cs
+++ WFGoodFood/GUI/FormLoading.cs
@@ -51,5 +51,7 @@ namespace WFGoodFood.GUI
             //Hide();
             //(LoginForm log = new LoginForm();
         }
+
+        
     }
 }
