1c3d25436d82b03abb01ca8d38861857e62b28b7
Yulia2120 <ket-dz@rambler.ru>
2022-05-24 19:34:32 +0300

    userControlManagement

---
+++ WFGoodFood/Control/UserControlManagement.Designer.cs
@@ -0,0 +1,45 @@
+﻿namespace WFGoodFood.Control
+{
+    partial class UserControlManagement
+    {
+        /// <summary> 
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором компонентов
+
+        /// <summary> 
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // UserControlManagement
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Name = "UserControlManagement";
+            this.Size = new System.Drawing.Size(880, 530);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+    }
+}
