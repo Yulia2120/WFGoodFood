92b0305dcce1d268a07a19e04765288329d281b3
Yulia2120 <ket-dz@rambler.ru>
2022-05-26 10:49:52 +0300

    Delete Form1

--- WFGoodFood/DataModel/Category.cs
+++ WFGoodFood/DataModel/Category.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,8 +10,11 @@ namespace WFGoodFood.DataModel
     public class Category
     {
         public int Id { get; set; }
-        public string Name { get; set; }
+        [StringLength(255)]
+        public string ProductName { get; set; }
+        [StringLength(255)]
         public string ImageUrl { get; set; }
+        [StringLength(2555)]
         public string Description { get; set; }
 
     }
---
+++ WFGoodFood/Migrations/202205260746406_category.cs
@@ -0,0 +1,28 @@
+﻿namespace WFGoodFood.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class category : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.Categories",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        ProductName = c.String(maxLength: 255),
+                        ImageUrl = c.String(maxLength: 255),
+                        Description = c.String(maxLength: 2555),
+                    })
+                .PrimaryKey(t => t.Id);
+            
+        }
+        
+        public override void Down()
+        {
+            DropTable("dbo.Categories");
+        }
+    }
+}
